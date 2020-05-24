b6323c5651172eb5e2e748c357a1bd1b249151a4
Carlos Reyes <reyesrico@hotmail.com>
2020-05-24 10:10:19 -0700

    Updates

---
+++ precache-manifest.75c422cea3e339599f94c11e76af32d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495611ee00324c611b1ce0258ab577a4",
+    "url": "/CovidCharts/index.html"
+  },
+  {
+    "revision": "12f04785baaada708d6c",
+    "url": "/CovidCharts/static/css/main.d68cf828.chunk.css"
+  },
+  {
+    "revision": "a8731c12d679ba926638",
+    "url": "/CovidCharts/static/js/2.66eb914f.chunk.js"
+  },
+  {
+    "revision": "2fce0e23a82f2574b85678dab91f58a2",
+    "url": "/CovidCharts/static/js/2.66eb914f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12f04785baaada708d6c",
+    "url": "/CovidCharts/static/js/main.ce1902d9.chunk.js"
+  },
+  {
+    "revision": "ce0f20a00f2fc32b04d7",
+    "url": "/CovidCharts/static/js/runtime-main.b517eac2.js"
+  }
+]);
